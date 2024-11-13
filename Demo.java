2163060a32a65bece38d3dd6c3a1b1788dba414c
Rakesh <rakeshKellurkar710gmail.com>
2024-11-13 11:16:11 +0530

    Add,Mul,Sub functions added

--- Demo.java
+++ Demo.java
@@ -3,4 +3,11 @@ public class Demo{
   int b = 20;
   int c = 31;
 
+	public static void main(String [] args){
+	
+		System.out.println(a*b);
+		System.out.println(b+c);
+		System.out.println(c-a);
+	}
+
 }
